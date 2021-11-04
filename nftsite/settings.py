f954c2598bb118fc669df35cfc3ed06d99b68882
Ami Zou <azou@ripple.com>
2021-11-04 12:49:12 -0700

    update settings

--- nftsite/settings.py
+++ nftsite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=s1tgebiw*4fzz@5^^6+-o_0cr5c@zh54s&j68z=w=#+0sy&s9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
